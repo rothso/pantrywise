30d3d58206fe86b5770e04729d5f5133b7d9561c
Rothanak So <rothanak.hello@gmail.com>
2025-09-07 10:10:44 -0400

    Proof-of-concept dummy UI

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { getHouseholdIdForUser } from '@/repositories/users';
 import { getRecipesByHousehold } from '@/repositories/recipes';
 import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
-import util from 'util';
 
 export default async function Page() {
   const supabase = await createClient();
@@ -18,7 +17,24 @@ export default async function Page() {
   const householdId = await getHouseholdIdForUser(user.id);
   const recipes = await getRecipesByHousehold(householdId);
 
-  console.log(util.inspect(recipes, { depth: null, colors: true }));
-
-  return <div>Hello world!</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-4 p-4 w-full">
+      <h1 className="text-2xl font-bold">PantryWise</h1>
+      <p className="text-lg">Recipes</p>
+      <ul className="list-disc">
+        {recipes.map((recipe) => (
+          <li key={recipe.recipeId} className="flex flex-col gap-2">
+            {recipe.name}
+            <ul className="list-disc pl-4 ml-4">
+              {recipe.recipeIngredients.map((ingredient) => (
+                <li key={ingredient.ingredients.name}>
+                  {ingredient.amount} {ingredient.ingredients.name}
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
