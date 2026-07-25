aaa23e5f60bec162ffcf8338deb7d139c6cbd32c
Rothanak So <rothanak.hello@gmail.com>
2026-07-25 16:30:15 -0400

    Bootstrap new Svelte frontend

---
+++ frontend/prettier.config.js
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+const config = {
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'none',
+  printWidth: 100,
+  plugins: ['prettier-plugin-svelte', 'prettier-plugin-tailwindcss'],
+  overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
+  tailwindStylesheet: './src/routes/layout.css'
+};
+
+export default config;
