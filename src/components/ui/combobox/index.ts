8d7e5f7be096b8789f1f7f2f4e7a1e49e0c41758
Rothanak So <rothanak.hello@gmail.com>
2026-08-16 12:51:44 -0400

    Remove old frontend code after monorepo migration

--- src/components/ui/combobox/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './combobox';
-export * from './combobox-content';
-export * from './combobox-empty';
-export * from './combobox-input';
-export * from './combobox-item';
-export * from './context';
-export * from './types';
